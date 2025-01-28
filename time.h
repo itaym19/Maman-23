fbf7596c3d6802ac6348478dbacf4f6f2751d927
itaym19 <openu1111@gmail.com>
2025-01-28 14:55:15 +0200

    Update time.h

--- time.h
+++ time.h
@@ -2,15 +2,7 @@ typedef struct {
     int year;
     int month;
     int day;
-} date;
-
-typedef struct {
     int hours;
     int minutes;
     int seconds;
-} today_time;
-
-typedef struct {
-    date date_instance;
-    today_time today_time_instance;
 } time;
