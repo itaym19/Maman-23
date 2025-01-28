45f1ddf9d919315d07f85154d155d5b9b44867c2
itaym19 <openu1111@gmail.com>
2025-01-28 16:52:30 +0200

    Update time.c

--- time.c
+++ time.c
@@ -1,2 +1,46 @@
 #incllude <stdio.h>
 
+char* later_time(const time* t1, const time* t2) {
+    /* Compare year */
+    if (t1.year > t2.year) {
+        return "t1";
+    } else if (t1.year < t2.year) {
+        return "t2";
+    }
+    
+    /* If years are equal, compare month */
+    if (t1.month > t2.month) {
+        return "t1";
+    } else if (t1.month < t2.month) {
+        return "t2";
+    }
+    
+    // If months are equal, compare day
+    if (t1.day > t2.day) {
+        return "t1";
+    } else if (t1.day < t2.day) {
+        return "t2";
+    }
+    
+    // If days are equal, compare hours
+    if (t1.hours > t2.hours) {
+        return "t1";
+    } else if (t1.hours < t2.hours) {
+        return "t2";
+    }
+    
+    // If hours are equal, compare minutes
+    if (t1.minutes > t2.minutes) {
+        return "t1";
+    } else if (t1.minutes < t2.minutes) {
+        return "t2";
+    }
+    
+    // If minutes are equal, compare seconds
+    if (t1.seconds > t2.seconds) {
+        return "t1";
+    } else {
+        return "t2";
+    }
+}
+
