0605b1b8baf28d9335d96d4dc4ff63a5510222dd
itaym19 <openu1111@gmail.com>
2025-01-30 13:01:29 +0200

    Update time.c

--- time.c
+++ time.c
@@ -15,28 +15,28 @@ char* later_time(const Time* t1, const Time* t2) {
         return "t2";
     }
     
-    // If months are equal, compare day
+    /* If months are equal, compare day */
     if (t1->day > t2->day) {
         return "t1";
     } else if (t1->day < t2->day) {
         return "t2";
     }
     
-    // If days are equal, compare hours
+    /* If days are equal, compare hours */
     if (t1->hours > t2->hours) {
         return "t1";
     } else if (t1->hours < t2->hours) {
         return "t2";
     }
     
-    // If hours are equal, compare minutes
+    /* If hours are equal, compare minutes */
     if (t1->minutes > t2->minutes) {
         return "t1";
     } else if (t1->minutes < t2->minutes) {
         return "t2";
     }
     
-    // If minutes are equal, compare seconds
+    /* If minutes are equal, compare seconds */
     if (t1->seconds > t2->seconds) {
         return "t1";
     } else {
