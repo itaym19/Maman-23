c4dd8ff5a5a69b2cb13e706887f6aac9944be53a
itaym19 <openu1111@gmail.com>
2025-01-30 13:40:25 +0200

    Update time.h

--- time.h
+++ time.h
@@ -8,3 +8,6 @@ typedef struct {
 } time;
 
 char* later_time(const Time* t1, const Time* t2);
+int get_days_in_month(int month);
+long long time_to_seconds(const Time* t);
+long long time_to_seconds_negative_years(const Time* t);
