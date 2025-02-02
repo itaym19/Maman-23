df524bcd7ba18f1ac8e3aac7f8ac015b42a49259
itaym19 <openu1111@gmail.com>
2025-02-02 22:20:35 +0200

    Update timediff.c

--- timediff.c
+++ timediff.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    long int date1_seconds;
-    long int date2_seconds;
-    char *later = malloc(3 * sizeof(char));
-    Time time1 = {1, 11, 28, 14, 30, 45};
-    Time time2 = {-2, 1, 21, 15, 20, 10};
-    
-    later = later_time(&time1, &time2);
-    date1_seconds = (time1.year >= 0)? time_to_seconds(&time1): time_to_seconds_negative_years(&time1);
-    date2_seconds = (time2.year >= 0)? time_to_seconds(&time2): time_to_seconds_negative_years(&time2);
-    
-    printf("\nThe later time is:\n%s", later);
-    printf("\ntime1 in seconds:\n%ld", date1_seconds);
-    printf("\ntime2 in seconds:\n%ld", date2_seconds);
+int main(int argc, char *argv[]) {
+    /* making sure there is exactly 1 file as an argument */
+    if (argc != 2) {
+        fprintf(stderr, "error getting text file: t.timediff ", argv[0]);
+        return 1;
+    }
 
+    /* opening file */
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    Time time1, time2;
+    while (fscanf(file, "%d %d %d %d %d %d %d %d %d %d %d %d", 
+                  &time1.year, &time1.month, &time1.day,
+                  &time1.hours, &time1.minutes, &time1.seconds,
+                  &time2.year, &time2.month, &time2.day,
+                  &time2.hours, &time2.minutes, &time2.seconds) == 12) {
+        printf("Time 1:\n");
+        print_time(&time1);
+        printf("\nTime 2:\n");
+        print_time(&time2);
+        time_difference_in_seconds(&time1, &time2);
+    }
+
+    fclose(file);
     return 0;
 }
